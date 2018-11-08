6956c91c69488cd8c7d3c8c363cef3b70c39ff79
AndreaPlm <palma.a.andrea@gmail.com>
2018-11-08 16:46:34 +0100

    changed in views, controller and services

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
   def search
+    @movies = SearchMovie.new.search("batman")
   end
 end
--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -1,3 +1,19 @@
 class SearchMovie
 
+Tmdb::Api.key(Rails.application.credentials[:api_key_tmdb])
+Tmdb::Api.language("fr")
+
+  def initialize
+
+  end
+
+  def search(string)
+    @search = Tmdb::Search.new
+    @search.resource('movie') # determines type of resource
+    @search.query(string) # the query to search against
+    @search.fetch # makes request
+
+  end
+
+
 end
