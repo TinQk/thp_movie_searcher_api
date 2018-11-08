04d0406997c707e169538e790ffca32b48ba20f8
Redcozmo <redcozmo@hotmail.com>
2018-11-08 18:42:02 +0100

    home + display

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'movies/search'
   post 'movies/result'
+  get '/', to: 'movies#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
